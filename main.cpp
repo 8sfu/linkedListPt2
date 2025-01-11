2b1c69977edca73e69d48773c5e303289bcbf92f
TH <HS@THs-MacBook-Pro.local>
2025-01-10 19:56:41 -0800

    fixed compat issues and set up skeleton in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,40 @@
 #include <cstring>
 #include <iomanip>
 #include <cmath>
-#include "student.h"
+#include "Student.h"
 
 using namespace std;
 
-int main(){
+bool cmp(char* input, const char* check){
+  if(!strcmp(input,(char*)check)){
+    return true;
+  } else {
+    return false;
+  }
+}
 
+int main(){
+  char* input = new char[20];
+  bool running = true;
+
+  cout << "Available commands - ADD,QUIT" << endl;
+  cin.getline(input,20);
+  if(cmp(input,"ADD")){
+  }else if(cmp(input,"QUIT")){
+    running = false;
+  }
+
+  while(running){
+    cout << "Available commands - ADD,QUIT,PRINT,DELETE,AVERAGE" << endl;
+    if(cmp(input,"ADD")){
+    }else if(cmp(input,"PRINT")){
+    }else if(cmp(input,"DELETE")){
+    }else if(cmp(input,"AVERAGE")){
+    }else if(cmp(input,"QUIT")){
+      running = false;
+    }
+  }
   
-  
+  delete[] input;
   return 0;
 }
