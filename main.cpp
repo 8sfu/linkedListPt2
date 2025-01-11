3a9b028d7a4485a027b3c0f7dd795273dcd394b4
TH <HS@THs-MacBook-Pro.local>
2025-01-11 02:43:15 -0800

    AAAAHHAHHH WE DID IT IT CAN DELETE NODES YESS SLEEP

--- main.cpp
+++ main.cpp
@@ -7,31 +7,6 @@
 
 using namespace std;
 
-/*Node* addNode(Node* listHead, Node* currentNode, Node* newNode){
-  int currentID = ((*currentNode).getStudent())->getID(); 
-  int checkID = ((*newNode).getStudent())->getID();
-  if(currentNode == listHead){
-    if(currentID > checkID){ //New node should be list head
-      newNode->setNext(currentNode); //Set new node to new list head
-      return newNode;
-    } else {
-      if(listHead->getNext() == nullptr){ //If the list is two long, add new node next
-	listHead->setNext(newNode);
-	return listHead;
-      } else { //Otherwise recurse thru to the end
-	addNode(listHead,listHead->getNext(),newNode);
-      }
-    }    
-  } else { //New node isn't list head
-    if(currentID < checkID){ //Check all nodes to see if new node goes after them
-      newNode->setNext(currentNode->getNext()); //Insert new node into list
-      currentNode->setNext(newNode);
-      return listHead;
-    }
-  }
-  return listHead;
-  }*/
-
 Node* addNode(Node* listHead, Node* currentNode, Node* newNode){
   int checkID = ((*newNode).getStudent())->getID();
   if(currentNode == listHead){ //setting to new list head
@@ -62,29 +37,17 @@ Node* deleteNode(Node* listHead, Node* currentNode, int* id){
   if(currentNode == listHead){
     int currentID = ((*currentNode).getStudent())->getID();
     if(currentID == checkID){ //new list head is next node
-      Node* placeholderNode = listHead->getNext();
-      listHead = placeholderNode;
-      return listHead;
-    } else if (currentNode->getNext() != nullptr) { //if second node exists
-      int nextCaseID = ((*(*listHead).getNext()).getStudent())->getID();
-      if(nextCaseID == checkID){ //if second node is to be deleted
-	listHead->setNext((*(*listHead).getNext()).getNext());
-	return listHead;
-      } else { //recurse thru list
-	deleteNode(listHead,listHead->getNext(),id);
-      }
-    } else { //delete whole list
-      return nullptr;
+      return listHead->getNext();
     }
-  } else if (currentNode->getNext() != nullptr) { //check other nodes in list
-    int currentID = ((*(*currentNode).getNext()).getStudent())->getID();
-    if(checkID == currentID){
+  }
+  if (currentNode->getNext() != nullptr) { //check next node
+    int currentNextID = ((*(*currentNode).getNext()).getStudent())->getID();
+    if(checkID == currentNextID){
       currentNode->setNext(((*(*currentNode).getNext()).getNext()));
       return listHead;
     }
-  } else {
-    cout << "There is no student with that name." << endl;
-  }
+  } //continue recursing through list
+  deleteNode(listHead,currentNode->getNext(),id);
   return listHead;
 }
 
@@ -127,7 +90,6 @@ int main(){
       cin.getline(input,20);
       if(cmp(input,"ADD")){
 	listHead = addNode(listHead,listHead,new Node(new Student()));
-	
       }else if(cmp(input,"PRINT")){
 	printList(listHead);
       }else if(cmp(input,"DELETE")){
@@ -136,7 +98,6 @@ int main(){
 	cin >> id;
 	cin.ignore();
 	listHead = deleteNode(listHead,listHead,&id);
-	
       }else if(cmp(input,"AVERAGE")){
       }else if(cmp(input,"QUIT")){
 	running = false;
